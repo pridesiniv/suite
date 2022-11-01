d660e2147db2bed1b9af44dda0bf804e04a46fdc
Niklaus <personality_iv@icloud.com>
2022-11-01 15:26:46 +0300

    Part2: Continuing to learn the Django-framework and creating a web-service

--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from .models import Bb
 
 
 def index(request):
-    return HttpResponse('Here will be your suite')
+    template = loader.get_template('index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
