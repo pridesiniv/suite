2fb528e2bb04d7b91d86841d0e63b23626a74f42
Niklaus <personality_iv@icloud.com>
2022-11-01 19:32:59 +0300

    Part3

--- bboard/models.py
+++ bboard/models.py
@@ -2,7 +2,22 @@ from django.db import models
 
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='Name')
+    content = models.TextField(null=True, blank=True, verbose_name='Description')
+    price = models.FloatField(null=True, blank=True, verbose_name='Price:')
+    published = models.DateField(auto_now_add=True, db_index=True, verbose_name='Published on:')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Rubric')
+
+    class Meta:
+        verbose_name_plural = 'Descriptions'
+        verbose_name = 'Description'
+        ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=30, db_index=True, verbose_name='Name')
+
+    class Meta:
+        verbose_name_plural = 'Rubrics'
+        verbose_name = 'Rubric'
+        ordering = ['name']
--- bboard/views.py
+++ bboard/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
 from .models import Bb
 
 
 def index(request):
-    template = loader.get_template('index.html')
-    bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))
+    bbs = Bb.objects.all()
+    return render(request, 'index.html', {'bbs': bbs})
