e328c761e7c87f4dfceecfe1473a8f2e2820b6eb
Niklaus <personality_iv@icloud.com>
2022-11-02 20:46:15 +0300

    Part4: Not much today as I was doing more theory

--- bboard/models.py
+++ bboard/models.py
@@ -17,6 +17,9 @@ class Bb(models.Model):
 class Rubric(models.Model):
     name = models.CharField(max_length=30, db_index=True, verbose_name='Name')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name_plural = 'Rubrics'
         verbose_name = 'Rubric'
--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from .models import Bb
+from .models import Bb, Rubric
 
 
 def index(request):
     bbs = Bb.objects.all()
     return render(request, 'index.html', {'bbs': bbs})
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics,
+                'current_rubric': current_rubric}
+    return render(request, '/by_rubric.html', context)
