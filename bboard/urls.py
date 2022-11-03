e602fc82673618c1accf52e7996e44c104634eea
Niklaus <personality_iv@icloud.com>
2022-11-03 16:07:23 +0300

    Part5: 1st commit of the day

--- bboard/urls.py
+++ bboard/urls.py
@@ -3,6 +3,6 @@ from .views import index, by_rubric
 
 
 urlpatterns = [
-    path('', index),
-    path('<int:rubric_id>/', by_rubric)
+    path('', index, name='index'),
+    path('<int:rubric_id>', by_rubric, name='by_rubric')
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -4,12 +4,15 @@ from .models import Bb, Rubric
 
 def index(request):
     bbs = Bb.objects.all()
-    return render(request, 'index.html', {'bbs': bbs})
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'index.html', context)
+
 
 def by_rubric(request, rubric_id):
     bbs = Bb.objects.filter(rubric=rubric_id)
     rubrics = Rubric.objects.all()
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics,
-                'current_rubric': current_rubric}
-    return render(request, '/by_rubric.html', context)
+               'current_rubric': current_rubric}
+    return render(request, 'by_rubric.html', context)
